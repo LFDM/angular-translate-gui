a91a307e5691a0fa3c8345cb67ed4e82b0513165
LFDM <1986gh@gmail.com>
2014-10-03 13:54:26 +0200

    Add ngAnimate

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,8 @@ angular.module('arethusaTranslateGuiApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'ngAnimate'
 ])
   .config(function ($routeProvider, $locationProvider) {
     $routeProvider
