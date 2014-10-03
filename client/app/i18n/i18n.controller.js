841ba6c964f42f6befd8d969b2f6308d803b801b
LFDM <1986gh@gmail.com>
2014-10-03 20:58:38 +0200

    Delete obsolete stuff

--- client/app/i18n/i18n.controller.js
+++ client/app/i18n/i18n.controller.js
@@ -8,30 +8,6 @@ angular.module('arethusaTranslateGuiApp').controller('I18nCtrl', [
       update: { method: 'PUT'}
     });
 
-    //var container1 = new Container({ name: 'landing', containers: [], values: [] });
-
-    //var val = {
-      //name: 'description',
-      //comment: 'One-liner to describe the framework',
-      //translations: []
-    //};
-
-    //var trsl1 = {
-      //lang: 'en',
-      //translation: 'A backend-independent client-side annotation framework'
-    //};
-
-    //var trsl2 = {
-      //lang: 'de',
-      //translation: 'Ein Backend-unabhängiges client-side Annotations-Framework'
-    //};
-
-    //val.translations.push(trsl1);
-    //val.translations.push(trsl2);
-    //container1.values.push(val);
-
-    //container1.$save();
-
     Container.query(function(res) {
       $scope.containers = res;
     });
