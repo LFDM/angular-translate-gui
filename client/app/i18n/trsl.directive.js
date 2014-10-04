de285b25d0a0218285421b87f3fb9c9bef87ceb4
LFDM <1986gh@gmail.com>
2014-10-04 11:36:15 +0200

    Refactor trsl directive

--- client/app/i18n/trsl.directive.js
+++ client/app/i18n/trsl.directive.js
@@ -3,23 +3,26 @@
 angular.module('arethusaTranslateGuiApp').directive('trsl', [
   '$timeout',
   function($timeout) {
+    var MAIN_LANGUAGE = 'en';
+    var DIRTY = 'dirty-bg-dark';
+    var CLEAN = 'clean-bg-dark';
+
+    function getEventName(main) {
+      return main ? 'mainChange' : 'trslChange';
+    }
+
+    function isMain(lang) {
+      return MAIN_LANGUAGE === lang;
+    }
+
     return {
       restrict: 'A',
-      link: function(scope, element) {
-        var MAIN_LANGUAGE = 'en';
-        var DIRTY = 'dirty-bg-dark';
-        var CLEAN = 'clean-bg-dark';
-
-        var main = MAIN_LANGUAGE === scope.trsl.lang;
-        var table = element.find('table');
-        var eventName = main ? 'mainChange' : 'trslChange';
+      link: function(scope) {
+        var main = isMain(scope.trsl.lang);
+        var eventName = getEventName(main);
 
         function switchClasses(newVal) {
-          var classes = [DIRTY, CLEAN];
-          var cl = newVal ? classes : classes.reverse();
-          table.addClass(cl[0]);
-          table.removeClass(cl[1]);
-
+          scope.statusClass = newVal ? DIRTY : CLEAN;
           scope.toggleIcon = newVal ? 'check' : 'remove';
         }
 
@@ -36,6 +39,14 @@ angular.module('arethusaTranslateGuiApp').directive('trsl', [
           changeStatus(true);
         }
 
+        function makeDirty() {
+          scope.trsl.dirty = true;
+        }
+
+        function makeClean() {
+          scope.trsl.dirty = false;
+        }
+
         scope.toggleStatus = function() {
           if (scope.trsl.dirty) {
             setClean();
@@ -44,18 +55,18 @@ angular.module('arethusaTranslateGuiApp').directive('trsl', [
           }
         };
 
-        if (!main) {
-          scope.$watch('trsl.dirty', switchClasses);
-          scope.addMainDirtyListener(scope, 'trsl');
-        } else {
-          scope.trsl.dirty = false;
-        }
-
         var timer;
         scope.trackChange = function() {
           if (timer) $timeout.cancel(timer);
           timer = $timeout(setClean, 1000);
         };
+
+        if (main) {
+          makeClean();
+        } else {
+          scope.$watch('trsl.dirty', switchClasses);
+          scope.$on('mainDirty', makeDirty);
+        }
       },
       templateUrl: 'app/i18n/trsl.directive.html'
     };
