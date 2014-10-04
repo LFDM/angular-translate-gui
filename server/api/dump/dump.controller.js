527d73e2ed545a26eb2a4357450f64215761fb95
LFDM <1986gh@gmail.com>
2014-10-04 23:16:21 +0200

    Add function to create a dumpable container

--- server/api/dump/dump.controller.js
+++ server/api/dump/dump.controller.js
@@ -3,6 +3,56 @@
 var _ = require('lodash');
 var Dump = require('./dump.model');
 var AdmZip = require('adm-zip');
+var Container = require('../container/container.model')
+var objPath = require('object-path');
+
+function addToLang(result, path, trsl, lang) {
+  var langContainer = result[lang];
+  if (!langContainer) langContainer = result[lang] = {};
+  objPath.set(langContainer, path, trsl);
+}
+
+function addTranslations(result, path, val) {
+  val.translations.forEach(function(trsl) {
+    addToLang(result, path, trsl.translation, trsl.lang)
+  });
+}
+
+function addValues(result, values, path) {
+  values.forEach(function(value) {
+    path.push(value.name);
+    addTranslations(result, path.join('.'), value);
+    path.pop();
+  });
+}
+
+function addContainers(result, containers, path) {
+  containers.forEach(function(container) {
+    path.push(container.name);
+    addValues(result, container.values, path);
+    addContainers(result, container.containers, path);
+    path.pop();
+  });
+}
+
+function generateDump(callback) {
+  Container.find(function(err, containers) {
+    var result = {};
+
+    // Take out the unnamespaced container
+    var i = _.findIndex(containers, function(el) {
+      return el.name === 'withoutNamespace';
+    });
+    var woNamespace = containers[i];
+    containers.splice(i, 1);
+
+    var path = [];
+    addValues(result, woNamespace.values, path);
+    addContainers(result, containers, path);
+
+    callback(result)
+  });
+}
 
 // Get list of dumps
 exports.index = function(req, res) {
