2df8ea7b3b33bb5b0d210a770cbfb6c33726af95
LFDM <1986gh@gmail.com>
2014-10-04 19:54:38 +0200

    Fix uid endpoint

--- server/api/uid/index.js
+++ server/api/uid/index.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
   var uid = mongoose.Types.ObjectId();
-  return res.json(uid, { uid: uid });
+  return res.json(200, { uid: uid });
 });
 
 module.exports = router;
