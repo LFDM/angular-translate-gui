d21eabbe427cfed2ca18025912d704b6ebd488ea
LFDM <1986gh@gmail.com>
2014-10-05 16:45:22 +0200

    Resize when index is shown

--- client/app/i18n/containers.directive.js
+++ client/app/i18n/containers.directive.js
@@ -5,8 +5,16 @@ angular.module('arethusaTranslateGuiApp').directive('containers', [
    return {
      restrict: 'A',
      scope: true,
-     link: function(scope, element, attrs) {
-
+     link: function(scope) {
+       scope.$watch('showIndex', function(newVal) {
+         if (newVal) {
+           scope.itemClass = 'small-12';
+           scope.containerClass = 'small-9';
+         } else {
+           scope.containerClass = 'small-12';
+           scope.itemClass = 'small-6';
+         }
+       });
      },
      templateUrl: 'app/i18n/containers.directive.html'
    };
