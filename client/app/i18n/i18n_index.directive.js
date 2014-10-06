ef2f7c8e436e62c2c818cb38dd8c28f2ddf951bd
LFDM <1986gh@gmail.com>
2014-10-06 11:28:31 +0200

    Update i18n index directive

--- client/app/i18n/i18n_index.directive.js
+++ client/app/i18n/i18n_index.directive.js
@@ -5,11 +5,43 @@ angular.module('arethusaTranslateGuiApp').directive('i18nIndex', [
    return {
      restrict: 'A',
      scope: true,
-     link: function(scope) {
-     },
      templateUrl: 'app/i18n/i18n_index.directive.html'
    };
   }
 ]);
 
+angular.module('arethusaTranslateGuiApp').directive('i18nIndexCollection', [
+  '$compile',
+  function($compile) {
+   return {
+     restrict: 'A',
+     scope: {
+       containers: '=i18nIndexCollection'
+     },
+     link: function(scope, element) {
+       var template = '\
+         <li ng-repeat="container in containers">\
+            <div i18n-index-item="container"/>\
+         </li>\
+       ';
+       element.append($compile(template)(scope));
+     }
+   };
+  }
+]);
 
+angular.module('arethusaTranslateGuiApp').directive('i18nIndexItem', [
+  function() {
+   return {
+     restrict: 'A',
+     scope: {
+       item: '=i18nIndexItem'
+     },
+     template: '\
+       <div>{{ item.name }}</div>\
+       <ul i18n-index-collection="item.values"/>\
+       <ul i18n-index-collection="item.containers"/>\
+     '
+   };
+  }
+]);
