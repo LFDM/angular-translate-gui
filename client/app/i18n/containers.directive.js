a2aeca49952b04c2f8edc4b171e9595b2fc197a7
LFDM <1986gh@gmail.com>
2014-10-06 11:59:34 +0200

    Try to cloak when layout changes

--- client/app/i18n/containers.directive.js
+++ client/app/i18n/containers.directive.js
@@ -1,19 +1,24 @@
 'use strict';
 
 angular.module('arethusaTranslateGuiApp').directive('containers', [
-  function() {
+  '$timeout',
+  function($timeout) {
    return {
      restrict: 'A',
      scope: true,
-     link: function(scope) {
+     link: function(scope, element) {
        scope.$watch('showIndex', function(newVal) {
          if (newVal) {
            scope.itemClass = 'small-12';
-           scope.containerClass = 'small-9';
+           scope.containerClass = 'small-9 large-10';
          } else {
            scope.containerClass = 'small-12';
            scope.itemClass = 'small-6';
          }
+         scope.cloak = true;
+         $timeout(function() {
+           scope.cloak = false;
+         }, 600);
        });
      },
      templateUrl: 'app/i18n/containers.directive.html'
