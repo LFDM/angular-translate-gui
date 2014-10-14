f75f3b43858fcb4a3db5367412aa9cd97f5aeca8
LFDM <1986gh@gmail.com>
2014-10-14 12:46:10 +0200

    Add expander to toggle index tree

--- client/app/i18n/i18n_index.directive.js
+++ client/app/i18n/i18n_index.directive.js
@@ -51,3 +51,41 @@ angular.module('arethusaTranslateGuiApp').directive('i18nIndexItem', [
    };
   }
 ]);
+
+angular.module('arethusaTranslateGuiApp').directive('i18nIndexExpander', [
+  function() {
+   function setExpander(scope) {
+     scope.expander = scope.expanded ? '-' : '+';
+   }
+
+   function isEmpty(item) {
+     return _.all([item.containers, item.values], function(el) {
+       return !(el && el.length > 0);
+     });
+   }
+
+   // This tracks if there are elements to expand only when the directive
+   // is compiled - it's therefore not live. As this directive will most
+   // likely be wrapped in an ngIf anyway, we might get away with this and
+   // spare us the additional watchers needed for this.
+   function init(scope) {
+     if (isEmpty(scope.item)) {
+       scope.expanded = true;
+     }
+     setExpander(scope);
+   }
+
+   return {
+     restrict: 'A',
+     link: function(scope) {
+       scope.toggle = function() {
+         scope.expanded = !scope.expanded;
+         setExpander(scope);
+       };
+
+       init(scope);
+     },
+     template: '<span class="clickable" ng-click="toggle()">{{ expander }}</span>'
+   };
+  }
+]);
