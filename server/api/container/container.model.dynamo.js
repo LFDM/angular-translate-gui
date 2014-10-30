dd8459fd520ee3e76efdbe74963bd0f740c614f6
LFDM <1986gh@gmail.com>
2014-10-30 18:27:13 +0100

    Update and use a nested schema in dynamo model

--- server/api/container/container.model.dynamo.js
+++ server/api/container/container.model.dynamo.js
@@ -2,14 +2,40 @@
 
 var vogels = require('vogels');
 
-var Container = vogels.define('Container', function(schema) {
-  schema.UUID('_id', { hashKey: true });
+
+function trslSchema(schema) {
+  schema.String('translation');
+  schema.String('lang');
+  schema.Boolean('dirty', { default: true });
+  schema.Date('updatedAt', { default: Date.now  });
+}
+
+function valueSchema(schema) {
+  schema.UUID('_id');
   schema.String('name');
   schema.String('comment');
+  schema.String('exampleUrl');
+  schema.String('example');
+  schema.Date('createdAt', { default: Date.now  });
   schema.Boolean('dirty', { default: true });
-  schema.List('values');
-  schema.List('containers');
-});
+  schema.List('translations', trslSchema);
+}
+
+function containerSchema(idOpts) {
+  return function(schema) {
+    schema.UUID('_id', idOpts);
+    schema.String('name');
+    schema.String('comment');
+    schema.Boolean('dirty', { default: true });
+    schema.Date('createdAt', { default: Date.now  });
+    schema.List('values', valueSchema);
+    schema.List('containers', { default: [] });
+  };
+}
+
+var Container = vogels.define('Container', containerSchema({
+  hashKey: true
+}));
 
 Container.createTable(function() {});
 module.exports = Container;
