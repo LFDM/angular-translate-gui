7491f1826c718d4c116b889c5f4cda25f330494c
LFDM <1986gh@gmail.com>
2014-10-30 18:26:55 +0100

    Update dynamo controller methods

--- server/api/container/container.controller.dynamo.js
+++ server/api/container/container.controller.dynamo.js
@@ -15,7 +15,7 @@ exports.index = function(req, res) {
 
 // Get a single container
 exports.show = function(req, res) {
-  Container.findById(req.params.id, function (err, container) {
+  Container.get(req.params.id, function (err, container) {
     if(err) { return handleError(res, err); }
     if(!container) { return res.send(404); }
     return res.json(container);
@@ -33,27 +33,24 @@ exports.create = function(req, res) {
 // Updates an existing container in the DB.
 exports.update = function(req, res) {
   if(req.body._id) { delete req.body._id; }
-  Container.findById(req.params.id, function (err, container) {
+  Container.get(req.params.id, function (err, container) {
     if (err) { return handleError(res, err); }
     if(!container) { return res.send(404); }
-    var updated = _.extend(container, req.body);
-    updated.save(function (err) {
+
+    container.attrs = _.extend(container.attrs, req.body);
+    console.log(req.body);
+    container.update(function(err) {
       if (err) { return handleError(res, err); }
-      return res.json(200, container);
-    });
+      return res.json(200, container.attrs);
+    })
   });
 };
 
 // Deletes a container from the DB.
 exports.destroy = function(req, res) {
-  console.log(req.params);
-  Container.findById(req.params.id, function (err, container) {
+  Container.destroy(req.params.id, function (err, container) {
     if(err) { return handleError(res, err); }
-    if(!container) { return res.send(404); }
-    container.remove(function(err) {
-      if(err) { return handleError(res, err); }
-      return res.send(204);
-    });
+    return res.send(204);
   });
 };
 
